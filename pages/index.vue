540781b6a4c841af39009654a88a2b26b6c603b1
geraldimanuel <geraldimanuelwijaya@gmail.com>
2025-01-21 23:28:57 +0700

    feat: add landing page of u-tapis harassment

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-      <h1>Welcome to the homepage</h1>
-      <AppAlert>
-        This is an auto-imported component
-      </AppAlert>
+        <section id="main-heading" class="max-w-screen-xl mx-auto px-6 py-10 flex justify-between">
+            <p class="font-bold text-4xl">U-Tapis - Sistem Online Pendeteksi <br>Penghinaan dan Pencemaran Nama Baik</p>
+            <img src="/img/logo_umn.png" alt="logo_umn" class="w-16 object-cover" />
+        </section>
+
+        <section id="hero-description" class="max-w-screen-xl mx-auto px-6 mt-5 flex flex-col gap-5">
+            <p class="font-medium text-2xl">Deteksi Pelanggaran Hukum dengan Mudah dan Cepat</p>
+            <p class="text-xl">Dengan U-Tapis Harassment, Anda dapat secara otomatis mendeteksi konten yang mengandung
+                penghinaan atau pencemaran
+                nama baik secara cepat dan akurat. Sistem ini dirancang untuk membantu melindungi reputasi, memastikan
+                keamanan digital, dan mendukung langkah hukum dengan bukti yang valid. U-Tapis menggabungkan teknologi
+                canggih dengan kemudahan penggunaan, memberikan solusi yang dapat diandalkan dalam dunia digital yang
+                semakin kompleks. Mulailah menjaga nama baik Anda sekarang juga!</p>
+        </section>
+
+        <section id="dropdown" class="max-w-screen-xl mx-auto px-6 mt-5 flex flex-col gap-5">
+            <div class="border border-slate-300 p-3 rounded-lg text-red-500 flex justify-between items-center">
+                <p>Cara Pakai U-Tapis Harassment</p>
+                <Icon name="icons8:angle-down" />
+            </div>
+            <div class="border border-slate-300 p-3 rounded-lg flex justify-between items-center">
+                <p>Sample Kalimat Untuk Dicoba</p>
+                <Icon name="icons8:angle-down" />
+            </div>
+        </section>
+
+        <section id="input" class="max-w-screen-xl mx-auto px-6 mt-5 mb-20 flex flex-col gap-5">
+            <p>Silahkan masukkan paragraf yang ingin dideteksi:</p>
+            <textarea class="border border-slate-300 p-3 rounded-lg resize-none" rows="4"></textarea>
+        </section>
+
     </div>
-</template>a
-  
+</template>
